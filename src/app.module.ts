d46ef74782d5afed34180b41b554b5a5bd1b4a03
XasanBaxtiyarovich <xasanavalov787@gmail.com>
2023-12-13 19:36:00 +0500

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
+import { Users } from './users/entities';
+import { Category } from './categories/entities';
 import { UsersModule } from './users/users.module';
-import { Users } from './users/entities/user.entity';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
@@ -30,11 +32,13 @@ import { Users } from './users/entities/user.entity';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [ Users ],
+      entities: [ Users, Category ],
       synchronize: true,
     }),
     
     UsersModule,
+    
+    CategoriesModule,
   ],
   controllers: [],
   providers: [],
