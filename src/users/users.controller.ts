e474213334021c4088dce1670f5d5f6ea6a86f7b
XasanBaxtiyarovich <xasanavalov787@gmail.com>
2023-12-13 12:32:05 +0500

    user and admin controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,7 +4,7 @@ import { UsersService } from './users.service';
 import { SignInDto, SignUpDto, UpdateDataDto, UpdatePassDto } from './dto';
 
 
-@Controller('user')
+@Controller('')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
@@ -25,17 +25,47 @@ export class UsersController {
   }
 
   // Find All Users
-  @Get('find')
+  @Get('find-users')
   find_users(): Promise<Object> {
     return this.usersService.find_users()
   }
 
+  // Find All Admins
+  @Get('find-admins')
+  find_admins(): Promise<Object> {
+    return this.usersService.find_admins()
+  }
+
+  // Find BY ID in Admin
+  @Get('find-admin/:id')
+  find_one_admin(
+    @Param('id') id: number
+  ): Promise<Object> {
+    return this.usersService.find_one_admin(id)
+  }
+
   // Find BY ID in User
-  @Get('find/:id')
+  @Get('find-user/:id')
   find_one_user(
     @Param('id') id: number
   ): Promise<Object> {
     return this.usersService.find_one_user(id)
+  }
+
+  // Find By Name In User
+  @Get('search-users/:name')
+  find_search_by_name_users(
+    @Param('name') name: string
+  ): Promise<Object> {
+    return this.usersService.find_search_by_name_users(name);
+  }
+
+    // Find By Name In User
+  @Get('search-admins/:name')
+  find_search_by_name_admins(
+    @Param('name') name: string
+  ): Promise<Object> {
+    return this.usersService.find_search_by_name_admins(name);
   }
 
   // Update User Data
@@ -62,5 +92,13 @@ export class UsersController {
     @Param('id') id: number
   ): Promise<Number> {
     return this.usersService.remove_user(id);
+  }
+
+  // Make Admin a User or Make a User an Administrator
+  @Get('admin-or-user/:id')
+  is_admin_user(
+    @Param('id') id: number
+  ): Promise<Object> {
+    return this.usersService.is_admin_user(id)
   }
 }
